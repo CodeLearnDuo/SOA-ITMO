13312256b1379e9cec81be7429e4408163aef33d
Ponomarev Ivan <i.ponomarev.1991@gmail.com>
2024-09-27 13:48:37 +0300

    init commit

---
+++ Ebay-Service/src/test/java/soa/duo/ebay_service/EbayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package soa.duo.ebay_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
